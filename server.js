842fde7ce3c1717d34b592a8c96f901c90a247c1
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-04-27 20:35:59 -0300

    Primeiro commit da API do produtos do mercado

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const produtos = [
+    { id: 1, nome: "Arroz", preco: 5.50},
+    { id: 2, nome: "Feijão", preco: 7.00},
+    { id: 3, nome: "Macarrão", preco: 4.00}
+]
+
+app.get('/produtos', (req, res) =>{
+    res.json(produtos)
+});
+
+app.get('/', (req, res) => {
+    res.send('Api funcionando...')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+    
+});
