f06a3aebdb4bfc9cce07acf2af10b1f2ecaa4d7c
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-04-28 16:38:04 -0300

    adicionado imagem salva na pasta uploads

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors')
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename:(req, file, cb)=> {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage })
 
 const app = express();
 
+app.post("/upload-produto", upload.single("imagem"), (req, res) => {
+    const { nome, preco } = req.body;
+
+    const imagem = req.file.filename;
+
+    res.json({
+        nome,
+        preco,
+        imagem
+    });
+});
+
 app.use(cors())
 app.use(express.json());
+
+app.use("/uploads", express.static("uploads"));
 
 const produtoRoutes = require('./routes/produtoRoutes');
 
